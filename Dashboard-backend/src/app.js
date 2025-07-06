57508e5900c6ac1c390b2f493049b4df9fd7c403
vishalnani21 <vishalnani21@gmail.com>
2025-07-06 12:55:13 +0530

    integrated frontend with backend

--- Dashboard-backend/src/app.js
+++ Dashboard-backend/src/app.js
@@ -9,8 +9,10 @@ const app=express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173' 
+  origin: ['http://localhost:5173', 'https://growthai-assessment1frontend.vercel.app/'],
+  credentials: true
 }));
+
 
 app.use(express.json());
 
