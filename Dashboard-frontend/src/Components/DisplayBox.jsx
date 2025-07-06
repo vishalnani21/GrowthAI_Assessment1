c858b2b2144557efbe7576b2219769051c9158fa
vishalnani21 <vishalnani21@gmail.com>
2025-07-06 13:10:07 +0530

    fixed issues in API

--- Dashboard-frontend/src/Components/DisplayBox.jsx
+++ Dashboard-frontend/src/Components/DisplayBox.jsx
@@ -10,7 +10,7 @@ const DisplayBox = () => {
   const[error,setError]=useState("");
   const handleRegenerate = async () => {
     try{
-     const response = await axios.get(`${URL_DATA}regenerate-headline`, {
+     const response = await axios.get(`${URL_DATA}/regenerate-headline`, {
         params: { name, location }
       });
    // console.log(response);
